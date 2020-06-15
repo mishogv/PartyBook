483d44e5a586da756acd1f096901bd093b273f7f
Mihail <mihogv@gmail.com>
2020-06-15 20:58:23 +0300

    Added some logic for booking

--- src/PartyBook.Services/BookService.cs
+++ src/PartyBook.Services/BookService.cs
@@ -1,6 +1,53 @@
 ﻿namespace PartyBook.Services
 {
+    using Microsoft.EntityFrameworkCore;
+    using PartyBook.Data;
+    using PartyBook.Data.Models;
+    using System;
+    using System.Threading.Tasks;
+
     public class BookService : IBookService
     {
+        private readonly ApplicationDbContext dbContext;
+
+        public BookService(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<int> CreateAsync(string telephoneNumber, int numberOfPeaople, DateTime when, string nightClubId, string userId, string message = "")
+        {
+            var bookRequest = new BookRequest()
+            {
+                NumberOfPeople = numberOfPeaople,
+                TelephoneNumber = telephoneNumber,
+                Message = message,
+                When = when,
+                NightClubId = nightClubId,
+                UserId = userId,
+            };
+
+            await this.dbContext.BookRequests.AddAsync(bookRequest);
+
+            await this.dbContext.SaveChangesAsync();
+
+            return bookRequest.Id;
+        }
+
+        public async Task<int> UpdateStatusAsync(bool status, int id, string userId)
+        {
+            var bookRequest = await this.dbContext.BookRequests.Include(x => x.NightClub).FirstOrDefaultAsync(x => x.Id == id);
+
+            if (bookRequest.NightClub.UserId != userId)
+            {
+                return -1;
+            }
+
+            this.dbContext.BookRequests.Update(bookRequest);
+
+            await this.dbContext.SaveChangesAsync();
+
+            return bookRequest.Id;
+        }
     }
 }
---
+++ src/PartyBook/Shared/Book/BookRequestCreateInputModel.cs
@@ -0,0 +1,26 @@
+﻿namespace PartyBook.ViewModels.Book
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+
+    public class BookRequestCreateInputModel
+    {
+        [Required]
+        public DateTime When { get; set; }
+
+        [Required]
+        [StringLength(10, MinimumLength = 10)]
+        public string TelephoneNumber { get; set; }
+
+        public string Message { get; set; }
+
+        [Range(1, int.MaxValue)]
+        public int NumberOfPeople { get; set; }
+
+        public bool IsApproved { get; set; } = false;
+
+        public bool IsRejected { get; set; } = false;
+
+        public string NightClubId { get; set; }
+    }
+}
